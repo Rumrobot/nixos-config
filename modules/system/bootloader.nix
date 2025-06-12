462410888b72bbe24ae8ce994578e073c1f6d0d3
Rumrobot <46647057+Rumrobot@users.noreply.github.com>
2025-06-12 02:19:29 +0200

    Simplify module parameters and remove cfg guards

---
+++ modules/system/bootloader.nix
@@ -0,0 +1,25 @@
+{ config, lib, ... }:
+
+let
+  hasNixos = config ? environment;
+  cfg = config.nixosConfig.system.bootloader;
+in {
+  options.nixosConfig.system.bootloader = {
+    enable = lib.mkEnableOption "Bootloader configuration" // { default = true; };
+    device = lib.mkOption {
+      type = lib.types.str;
+      default = "/dev/vda";
+      description = "Target device for GRUB";
+    };
+  };
+
+  config = lib.mkMerge [
+    (lib.mkIf hasNixos {
+      boot.loader.grub = {
+        enable = true;
+        device = cfg.device;
+        useOSProber = true;
+      };
+    })
+  ];
+}
