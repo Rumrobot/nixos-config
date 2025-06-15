1d5232073121f64426ef6472edac4944c8a19b51
Rumrobot <46647057+Rumrobot@users.noreply.github.com>
2025-06-16 00:17:26 +0200

    fix: Enable swww support for sunpaper

--- modules/home/wm/sunpaper.nix
+++ modules/home/wm/sunpaper.nix
@@ -7,7 +7,25 @@
   wallpaper = "25th_hour";
   lat = "55.83333N";
   long = "12.33333E";
+
+  swwwFps = 5;
+  swwwStep = 5;
 in {
+  # Add swww support
+  nixpkgs.overlays = [
+    (self: super: {
+      sunpaper = super.sunpaper.overrideAttrs (prev: rec {
+        patchPhase =
+          prev.patchPhase
+          + ''
+            substituteInPlace sunpaper.sh --replace 'swww_enable="false"' 'swww_enable="true"'
+            substituteInPlace sunpaper.sh --replace 'swww_fps="[0-9]\+"' 'swww_fps="${swwwFps}"'
+            substituteInPlace sunpaper.sh --replace 'swww_step="[0-9]\+"' 'swww_step="${swwwStep}"'
+          '';
+      });
+    })
+  ];
+
   home.packages = with pkgs; [sunpaper jq];
 
   xdg.dataFile."wallpapers/${wallpaper}".source = ../../../users/${username}/wallpapers/${wallpaper};
