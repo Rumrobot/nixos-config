f825e0a6f4d448dfa05668f7e2f163d54a3a860d
Rumrobot <46647057+Rumrobot@users.noreply.github.com>
2026-01-24 00:28:43 +0100

    chore: Updated to new git options

--- modules/programs/cli/git.nix
+++ modules/programs/cli/git.nix
@@ -12,20 +12,18 @@
       enable = true;
       lfs.enable = true;
 
-      # Should be changed to "settings" when nixpkgs is updated
-      # and the user and signing parts moved out into programs.git
-      extraConfig = {
-        user = {
-          name = "Rumrobot";
-          email = "46647057+Rumrobot@users.noreply.github.com";
-        };
+      user = {
+        name = "Rumrobot";
+        email = "46647057+Rumrobot@users.noreply.github.com";
+      };
 
-        signing = {
-          format = "ssh";
-          key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDul1CyPupXyjX+YEoh5y49GxpJr2VLQ1dsn1JB5Qk2c";
-          signByDefault = true;
-        };
+      signing = {
+        format = "ssh";
+        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDul1CyPupXyjX+YEoh5y49GxpJr2VLQ1dsn1JB5Qk2c";
+        signByDefault = true;
+      };
 
+      settings = {
         init.defaultBranch = "main";
       };
     };
