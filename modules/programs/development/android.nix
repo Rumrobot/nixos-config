164c545c4654ec541d214ff675e1128b87b6de19
Rumrobot <46647057+Rumrobot@users.noreply.github.com>
2026-01-24 00:13:49 +0100

    fix: Updated installation of adb to android-tools

--- modules/programs/development/android.nix
+++ modules/programs/development/android.nix
@@ -1,12 +1,12 @@
-{delib, host, homeManagerUser, ...}:
+{delib, host, pkgs, homeManagerUser, ...}:
 delib.module {
   name = "programs.development.android";
 
   options = delib.singleEnableOption host.developmentFeatured;
 
   nixos.ifEnabled = {
-    programs.adb.enable = true;
-
     users.users.${homeManagerUser}.extraGroups = ["adbusers" "kvm"];
   };
+
+  home.ifEnabled.home.packages = [pkgs.android-tools];
 }
