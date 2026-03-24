ecbc8e7fd63979dace566c089059f7b0dd995bb7
Rumrobot <46647057+Rumrobot@users.noreply.github.com>
2026-03-24 10:17:27 +0000

    chore: Bump Anycubic SlicerNext version

--- modules/programs/desktop/slicers/anycubic-slicernext.nix
+++ modules/programs/desktop/slicers/anycubic-slicernext.nix
@@ -1,4 +1,9 @@
-{ delib, host, pkgs, ...}:
+{
+  delib,
+  host,
+  pkgs,
+  ...
+}:
 delib.module {
   name = "programs.desktop.slicers.anycubic-slicernext";
 
@@ -7,9 +12,9 @@ delib.module {
   home.ifEnabled.services.flatpak.packages = [
     rec {
       appId = "com.anycubic.SlicerNext";
-      sha256 = "12a8rs0qcwkrp8dbzcsbnwpg71jqaqw6srijpp82zacp8gimr87p";
+      sha256 = "08swdgyy3y40ri8xgwfzcfr5b2n4i5zldvpwzflsnklmnml7z5k9";
       bundle = "${pkgs.fetchurl {
-        url = "https://github.com/msax81/AnycubiceSlicerNextFlatpak/releases/download/v1.3.995/AnycubicSlicerNext-1.3.995.flatpak";
+        url = "https://github.com/msax81/AnycubiceSlicerNextFlatpak/releases/download/v1.3.96/AnycubicSlicerNext-1.3.96.flatpak";
         inherit sha256;
       }}";
     }
