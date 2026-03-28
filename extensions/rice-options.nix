d34e6149cca7e93b1030c4b02d06eed1065633a4
Rumrobot <46647057+Rumrobot@users.noreply.github.com>
2026-03-28 18:01:48 +0000

    style: Formatted rice-options.nix

--- extensions/rice-options.nix
+++ extensions/rice-options.nix
@@ -1,22 +1,20 @@
-{ delib, ... }:
-let
-  inherit (delib)
+{delib, ...}: let
+  inherit
+    (delib)
     listOfOption
     package
     noDefault
     strOption
     packageOption
     intOption
-    pathOption
+    # pathOption
     ;
 in
-delib.extension {
-  name = "extend-rice-options";
-  description = "Extends Denix rice options";
+  delib.extension {
+    name = "extend-rice-options";
+    description = "Extends Denix rice options";
 
-  libExtension =
-    _: _: prev:
-    let
+    libExtension = _: _: prev: let
       themeOption = {
         name = noDefault (strOption null);
         package = noDefault (packageOption null);
@@ -26,20 +24,21 @@ delib.extension {
         inherit (themeOption) name package;
         size = noDefault (intOption null);
       };
-    in
-    {
-      riceSubmoduleOptions = prev.riceSubmoduleOptions // {
-        packages = listOfOption package [ ];
-        fonts = {
-          sans = themeSizeOption;
-          serif = themeSizeOption;
-          monospace = themeSizeOption;
-          emoji = themeSizeOption;
+    in {
+      riceSubmoduleOptions =
+        prev.riceSubmoduleOptions
+        // {
+          packages = listOfOption package [];
+          fonts = {
+            sans = themeSizeOption;
+            serif = themeSizeOption;
+            monospace = themeSizeOption;
+            emoji = themeSizeOption;
+          };
+          # cursor = themeSizeOption;
+          # icons = themeOption;
+          # gtkThemeName = noDefault (strOption null);
+          # wallpaper = noDefault (pathOption null);
         };
-        # cursor = themeSizeOption;
-        # icons = themeOption;
-        # gtkThemeName = noDefault (strOption null);
-        # wallpaper = noDefault (pathOption null);
-      };
     };
-}
+  }
