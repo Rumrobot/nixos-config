da2eb0db5756a991b307610c8e85cac085f34577
Rumrobot <46647057+Rumrobot@users.noreply.github.com>
2026-06-02 12:25:57 +0200

    chore: bumped zen keybinds version (?)

--- modules/programs/browsers/zen.nix
+++ modules/programs/browsers/zen.nix
@@ -128,7 +128,7 @@ delib.module {
         ];
 
         # Keyboard shortcuts
-        keyboardShortcutsVersion = 18; # Pin to detect regressions on zen updates
+        keyboardShortcutsVersion = 19; # Pin to detect regressions on zen updates
         keyboardShortcuts = [
           {
             id = "key_quitApplication";
