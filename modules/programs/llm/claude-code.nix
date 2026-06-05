f906b353d03c6d7218139c4c73566c040fab81e0
Rumrobot <46647057+Rumrobot@users.noreply.github.com>
2026-06-05 19:11:05 +0200

    feat: added superforms and code-review plugins to claude-code

--- modules/programs/llm/claude-code.nix
+++ modules/programs/llm/claude-code.nix
@@ -9,5 +9,25 @@ delib.module {
 
   options = delib.singleEnableOption (host.llmFeatured && host.developmentFeatured);
 
-  home.ifEnabled.home.packages = [pkgs.claude-code];
+  home.ifEnabled = {
+    programs.claude-code = {
+      enable = true;
+
+      plugins = [
+        (pkgs.fetchFromGitHub {
+          owner = "obra";
+          repo = "superpowers";
+          rev = "6fd4507659784c351abbd2bc264c7162cfd386dc";
+          hash = "sha256-P/FD8HTQO+QzvMe3A/B2v2vjs8T6ZmIYH3MPp79dSzo=";
+        })
+
+        "${pkgs.fetchFromGitHub {
+          owner = "anthropics";
+          repo = "claude-code";
+          rev = "feabcc3c2b9abf057a3721cf5427f8f50d75618f";
+          hash = "sha256-8m9s/OpebAkl50/fM6VynPKjVejHSnDYmrPyivwEL3U=";
+        }}/plugins/code-review"
+      ];
+    };
+  };
 }
