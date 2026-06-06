fb2836e651c0a714fca26cd397b70a549f4fbb83
Rumrobot <46647057+Rumrobot@users.noreply.github.com>
2026-06-06 17:15:30 +0200

    fix: ssdp firewall rules

--- modules/toplevel/hardware/networking.nix
+++ modules/toplevel/hardware/networking.nix
@@ -2,6 +2,7 @@
   delib,
   host,
   homeManagerUser,
+  pkgs,
   ...
 }:
 delib.module {
@@ -19,6 +20,16 @@ delib.module {
           2021 # Bambu Lab SSDP discovery
           5353 # mDNS (Avahi)
         ];
+
+        # Multicast SSDP UDP
+        extraPackages = [pkgs.ipset];
+        extraCommands = ''
+          if ! ipset --quiet list upnp; then
+            ipset create upnp hash:ip,port timeout 3
+          fi
+          iptables -A OUTPUT -d 239.255.255.250/32 -p udp -m udp --dport 1900 -j SET --add-set upnp src,src --exist
+          iptables -A nixos-fw -p udp -m set --match-set upnp dst,dst -j nixos-fw-accept
+        '';
       };
     };
 
