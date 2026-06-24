466db1cc8df656ebbb92bdb1da398a91beaa451f
Rumrobot <46647057+Rumrobot@users.noreply.github.com>
2026-06-24 13:50:17 +0200

    feat: added codex

---
+++ modules/programs/llm/codex.nix
@@ -0,0 +1,35 @@
+{
+  delib,
+  host,
+  pkgs,
+  ...
+}:
+delib.module {
+  name = "programs.llm.codex";
+
+  options = delib.singleEnableOption (host.llmFeatured && host.developmentFeatured);
+
+  home.ifEnabled = {
+    programs.codex = {
+      enable = true;
+
+      plugins = [
+        (pkgs.fetchFromGitHub {
+          name = "superpowers";
+          owner = "obra";
+          repo = "superpowers";
+          rev = "6fd4507659784c351abbd2bc264c7162cfd386dc";
+          hash = "sha256-P/FD8HTQO+QzvMe3A/B2v2vjs8T6ZmIYH3MPp79dSzo=";
+        })
+
+        (pkgs.fetchFromGitHub {
+          name = "ponytail";
+          owner = "DietrichGebert";
+          repo = "ponytail";
+          rev = "45f7d2f83fb430a65fd512a98ad7b14d79e06636";
+          hash = "sha256-BAwav7tf6RuHZ/A7TF/1k1TXWhYAdshlsYB3LbdgUD8=";
+        })
+      ];
+    };
+  };
+}
