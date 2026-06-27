1a001df1c8b948d2f4a120b5c47b4a793b546141
Rumrobot <46647057+Rumrobot@users.noreply.github.com>
2026-06-27 14:56:02 +0200

    fix: qidi-studio disabled by default

--- modules/programs/desktop/slicers/qidi-studio.nix
+++ modules/programs/desktop/slicers/qidi-studio.nix
@@ -1,13 +1,12 @@
 {
   delib,
-  host,
   pkgs,
   ...
 }:
 delib.module {
   name = "programs.desktop.slicers.qidi-studio";
 
-  options = delib.singleEnableOption host.guiFeatured;
+  options = delib.singleEnableOption false;
 
   home.ifEnabled.home.packages = [pkgs.qidi-studio];
 }
