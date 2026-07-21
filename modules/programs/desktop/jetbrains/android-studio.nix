d2ebfa807db9a79374dd149dd84effefb06478a0
Rumrobot <46647057+Rumrobot@users.noreply.github.com>
2026-07-21 13:17:25 +0200

    fix: android-studio now includes the android sdk

--- modules/programs/desktop/jetbrains/android-studio.nix
+++
@@ -1,8 +0,0 @@
-{ delib, host, pkgs, ...}:
-delib.module {
-  name = "programs.desktop.jetbrains.android-studio";
-
-  options = delib.singleEnableOption (host.guiFeatured && host.developmentFeatured);
-
-  home.ifEnabled.home.packages = [pkgs.android-studio];
-}
--- modules/programs/development/android.nix
+++ modules/programs/development/android.nix
@@ -1,12 +1,45 @@
-{delib, host, pkgs, homeManagerUser, ...}:
-delib.module {
-  name = "programs.development.android";
+{
+  delib,
+  host,
+  pkgs,
+  lib,
+  homeManagerUser,
+  ...
+}: let
+  android =
+    (import pkgs.path {
+      inherit (pkgs.stdenv.hostPlatform) system;
+      config = {
+        allowUnfree = true;
+        android_sdk.accept_license = true;
+      };
+    }).androidenv.composeAndroidPackages {
+      includeNDK = true;
+      includeEmulator = true;
+      includeSystemImages = true;
+      systemImageTypes = ["google_apis_playstore"];
+      abiVersions = ["x86_64"];
+    };
 
-  options = delib.singleEnableOption host.developmentFeatured;
+  sdkRoot = "${android.androidsdk}/libexec/android-sdk";
+in
+  delib.module {
+    name = "programs.development.android";
 
-  nixos.ifEnabled = {
-    users.users.${homeManagerUser}.extraGroups = ["adbusers" "kvm"];
-  };
+    options = delib.singleEnableOption host.developmentFeatured;
 
-  home.ifEnabled.home.packages = [pkgs.android-tools];
-}
+    nixos.ifEnabled.users.users.${homeManagerUser}.extraGroups = ["kvm"];
+
+    home.ifEnabled = {
+      home.packages = with pkgs;
+        [flutter jdk21 android.androidsdk android-tools]
+        ++ lib.optional host.guiFeatured (android-studio.withSdk android.androidsdk);
+
+      home.sessionVariables = {
+        ANDROID_HOME = sdkRoot;
+        ANDROID_SDK_ROOT = sdkRoot;
+        JAVA_HOME = "${pkgs.jdk21}";
+        FLUTTER_SUPPRESS_ANALYTICS = "true"; # dart needs `dart --disable-analytics` once
+      };
+    };
+  }
