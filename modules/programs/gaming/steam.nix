da4a0c0c67c8d2522f14cac211fd89b11548ea71
Rumrobot <46647057+Rumrobot@users.noreply.github.com>
2026-08-08 19:27:11 +0200

    fix: Satisfactory with steam using Proton GE

--- modules/programs/gaming/steam.nix
+++ modules/programs/gaming/steam.nix
@@ -1,6 +1,7 @@
 {
   delib,
   host,
+  pkgs,
   ...
 }:
 delib.module {
@@ -8,10 +9,15 @@ delib.module {
 
   options = delib.singleEnableOption host.gamingFeatured;
 
+  # Satisfactory needs to use Proton GE with these launch options to work properly on wayland (niri):
+  # PROTON_ENABLE_WAYLAND=1 WAYLANDDRV_PRIMARY_MONITOR=DP-2 %command%
+
   nixos.ifEnabled = {
     programs.steam = {
       enable = true;
       remotePlay.openFirewall = true;
+      extraCompatPackages = [pkgs.proton-ge-bin];
+      extraPackages = [pkgs.mangohud];
     };
 
     programs.gamescope.enable = true;
