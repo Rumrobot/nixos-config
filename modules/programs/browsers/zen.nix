fdde6e903f19aa4d4453b3aa523c69bab341f8df
Rumrobot <46647057+Rumrobot@users.noreply.github.com>
2026-09-02 12:44:22 +0200

    chore: update zen shortcuts version

--- modules/programs/browsers/zen.nix
+++ modules/programs/browsers/zen.nix
@@ -132,7 +132,7 @@ delib.module {
         ];
 
         # Keyboard shortcuts
-        keyboardShortcutsVersion = 19; # Pin to detect regressions on zen updates
+        keyboardShortcutsVersion = 20; # Pin to detect regressions on zen updates
         keyboardShortcuts = [
           {
             id = "key_quitApplication";
